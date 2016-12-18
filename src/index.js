36b4b049d5f23b6c27f95c89f9de466bc78e7b2f
march-dave <ysangy@gmail.com>
2016-12-18 14:56:23 -0500

    production webpack run

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router'
 // import { history } from 'react-router/lib/BrowserHistory';
 
 
-import App from './components/app'
+import App from './components/App'
+import Dashboard from './components/Dashboard'
+import About from './components/About'
+import Join from './components/Join'
+import Login from './components/Login'
+
 import './styles/app.scss'
 
 ReactDOM.render(
@@ -19,3 +24,23 @@ ReactDOM.render(
   </Router>,
   document.getElementById('main')
 )
+
+// import React from 'react'
+// import { render } from 'react-dom'
+// import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+//
+// import App from './components/App'
+// import Dashboard from './components/Dashboard'
+// import About from './components/About'
+// import Join from './components/Join'
+//
+// render(
+//   <Router history={browserHistory}>
+//     <Route path="/" component={App}>
+//       <IndexRoute component={Dashboard} />
+//       <Route path="tenants" component={About} />
+//       <Route path="properties" component={Join} />
+//     </Route>
+//   </Router>,
+//   document.getElementById('main')
+// )
