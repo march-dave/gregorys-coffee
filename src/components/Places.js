d785f280518672d9bec05b7db777a3f035c703b8
march-dave <ysangy@gmail.com>
2016-12-21 18:15:55 -0500

    map marker

--- src/components/Places.js
+++ src/components/Places.js
@@ -11,22 +11,22 @@ class Places extends Component {
     }
   }
 
-  componentDidMount() {
-     const url = 'https://api.foursquare.com/v2/venues/search?ll=40.7575285,-73.9884469&oauth_token=0DWMXELULH1PCZUJVTPBZ5ISSSD30DIXN2WZGRNEU0KZW23G&v=20161209'
-     superagent
-     .get(url)
-     .query(null)
-     .set('Accept', 'text/json')
-     .end((err, res) => {
-       const venues = res.body.response.venues;
-       console.log(venues);
-
-       this.setState({
-         venues: venues
-       })
-
-     })
- }
+ //  componentDidMount() {
+ //     const url = 'https://api.foursquare.com/v2/venues/search?ll=40.7575285,-73.9884469&oauth_token=0DWMXELULH1PCZUJVTPBZ5ISSSD30DIXN2WZGRNEU0KZW23G&v=20161209'
+ //     superagent
+ //     .get(url)
+ //     .query(null)
+ //     .set('Accept', 'text/json')
+ //     .end((err, res) => {
+ //       const venues = res.body.response.venues;
+ //       console.log(venues);
+ //
+ //       this.setState({
+ //         venues: venues
+ //       })
+ //
+ //     })
+ // }
 
 render() {
 
--- src/components/app.js
+++ src/components/app.js
@@ -14,19 +14,33 @@ class App extends React.Component {
 
      this.state = {
        conactName: 'Contact Me',
-       venues: [],
-       search: "New York",
-       search2: ""
+       venues: []
      }
-     this.upDataSearch = this.upDataSearch.bind(this);
+    //  this.upDataSearch = this.upDataSearch.bind(this);
    }
 
-   upDataSearch(e) {
-    console.log('upDataSearch', e.target.value)
-    this.setState( {
-      search2: e.target.value
-    } )
-  }
+   componentDidMount() {
+     const url = 'https://api.foursquare.com/v2/venues/search?ll=40.7575285,-73.9884469&oauth_token=0DWMXELULH1PCZUJVTPBZ5ISSSD30DIXN2WZGRNEU0KZW23G&v=20161209'
+     superagent
+     .get(url)
+     .query(null)
+     .set('Accept', 'text/json')
+     .end((err, res) => {
+       const venues = res.body.response.venues;
+       console.log(venues);
+
+       this.setState({
+         venues: venues
+       })
+     })
+   }
+
+  //  upDataSearch(e) {
+  //   console.log('upDataSearch', e.target.value)
+  //   this.setState( {
+  //     search2: e.target.value
+  //   } )
+  // }
 
   render() {
     const location = {
